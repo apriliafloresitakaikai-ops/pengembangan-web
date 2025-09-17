9d3bf2583207bca6a11a94087e39d4169db65f62
Networking BPVP Sorong <apriliafloresitakaikai@gmail.com>
2025-09-17 15:42:03 +0900

    kondisi

---
+++ kodisi perulangan/for.js
@@ -0,0 +1,12 @@
+let jumlah = 0;
+
+console.log("Bilangan genap dari 1 hingga 20");
+
+for (let i =1; i <= 20; i++) {
+ if (i % 2 === 0) {
+   console.log(i);
+   jumlah += i;
+ }
+}
+
+console.log("jumblah semua bilangan genap tersenodebut adalah:,jumblah");
---
+++ kodisi perulangan/if-else.js
@@ -0,0 +1,11 @@
+let nilai = 83;
+
+if(nilai >=90)  {
+    console.log("Nilai kamu A");
+}   else if (nilai >= 80)  {
+    console.log("Nilai kamu B");
+}   else if (nilai >= 70)  {
+    console.log("Nilai kamu c");
+}   else {
+    console.log("Nilai kami D")
+}
